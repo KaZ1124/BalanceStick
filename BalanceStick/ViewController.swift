0e6055934a6025eec53dcac3a27a4f9eec757e39
KaZ1124 <HiraKazu1124@gmail.com>
2018-04-10 06:04:59 +0900

    　1回目のコミット

---
+++ BalanceStick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BalanceStick
+//
+//  Created by 平川一樹 on 2018/04/10.
+//  Copyright © 2018年 平川 一樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
